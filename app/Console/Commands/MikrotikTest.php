a856a4cf4ac92a997fa773afbba6ec396154cbdb
kasirye Nicholas <kasiryenicholas@gmail.com>
2026-04-16 11:44:16 +0300

    adding on router os API conn

---
+++ app/Console/Commands/MikrotikTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+use Illuminate\Console\Command;
+use RouterOS\Client;
+use RouterOS\Query;
+
+
+
+class MikrotikTest extends Command
+{
+    protected $signature = 'mikrotik:test';
+    protected $description = 'Test MikroTik Router API connection';
+
+    public function handle()
+    {
+        $client = new Client([
+            'host' => '192.168.100.1',
+            'user' => 'admin',
+            'pass' => 'fROsw@Ve8Is&A6&trUWR',
+            'port' => 8728,
+        ]);
+
+        $query = new Query('/system/identity/print');
+
+        $response = $client->query($query)->read();
+
+        print_r($response);
+    }
+}
+
+
+
+// ======================================================================
+// #[Signature('app:mikrotik-test')]
+// #[Description('Command description')]
+
+// #[Signature('mikrotik:test')]
+// #[Description('Test MikroTik Router API connection')]
+// class MikrotikTest extends Command
+// {
+//     /**
+//      * Execute the console command.
+//      */
+//     public function handle()
+    
+//         //
+//     {
+//         $client = new Client([
+//             'host' => '192.168.100.1',
+//             'user' => 'admin',
+//             'pass' => 'fROsw@Ve8Is&A6&trUWR',
+//             'port' => 8728,
+//         ]);
+
+//         $query = new Query('/system/identity/print');
+
+//         $response = $client->query($query)->read();
+
+//         print_r($response);
+//     }
+// }
